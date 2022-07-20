b994d232f76f1cf31b551c3f79a580adf62746a0
Mary Frye <maryfrye@Marys-MacBook-Pro.local>
2022-07-20 15:24:17 -0400

    Added bread index at index.jsx

--- views/layouts/index.jsx
+++ views/layouts/index.jsx
@@ -3,11 +3,20 @@ const Default = require('./layouts/Default')
 
 function Index ({breads})  {
     return (
-      <Default>
+    <Default>
         <h2>Index Page</h2>
-        <p>I have {breads[0].name} bread!</p>
-      </Default>
-    )
-}
-
+        {/* <p>I have {breads[0].name} bread!</p> */}
+         <ul>
+          {
+            breads.map((bread, index)=> {
+                return (<li key={index}>
+                    <a>
+                    {bread.name}
+                    </a>
+                </li>)
+            })
+          }
+        </ul> 
+    </Default>
+)}
 module.exports = Index
