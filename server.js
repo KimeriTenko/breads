008a60cb9de1e3a2e52f37fb1045d6ab134a6c58
Mary Frye <maryfrye@Marys-MacBook-Pro.local>
2022-07-19 20:54:01 -0400

    Added breads_controller and server.js edit

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-//Dependencies
-const express = require('express')
-
 // CONFIGURATION
 require('dotenv').config()
+const express = require('express')
+const app = express()
 const PORT = process.env.PORT
 console.log(PORT)
 
@@ -10,6 +9,10 @@ console.log(PORT)
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
 })
+
+//Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
 
 // LISTEN
 app.listen(PORT, () => {
