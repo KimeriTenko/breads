69cf6ca1a2f1f8e865a63a12971d6b4c2cc2c40c
Mary Frye <deimospho@gmail.com>
2022-08-04 14:36:29 -0400

    EDIT index.jsx

--- views/index.jsx
+++ views/index.jsx
@@ -4,9 +4,23 @@ const Default = require('./layouts/Default')
 function Index ({breads, bakers, title})  {
     return (
         <Default title={title}>
-            <h2>Index Page</h2>
-            <h3><Bakers></h3>
-            <ul>
+          <h2>Index Page</h2>
+          <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
+          </div>
+          {
+            breads.map((bread, index)=> {
+              return (
+                <li key={index}>
+                  <a href={`/breads/${index}`}>
+                    {bread.name}
+                  </a>
+                </li>
+              )
+            })
+          }
+          <h3>Bakers</h3>
+          <ul>
           {
               bakers.map((baker)=> {
                   return (
@@ -16,10 +30,10 @@ function Index ({breads, bakers, title})  {
                   )
               })
           }
-      </ul>
-      <h3>Breads</h3>
-        <div className="newButton">
-          <a href="/breads/new"><button>Add a new bread</button></a>
+          </ul>
+          <h3>Breads</h3>
+          <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
         </div>
         {
           breads.map((bread, index)=> {
